0f395b1d66212313195c3b603a5050e95d68447c
kos-vkg <kvertenov@mail.ru>
2020-11-24 23:24:37 +0300

    loaded files

---
+++ src/Мain.java
@@ -0,0 +1,10 @@
+public class Мain {
+    public static void main(String[] args) {
+        int bonus_for_mile =  20;
+        long ticket_price = 20000;
+        long bonus = ticket_price / bonus_for_mile;
+
+        System.out.println(bonus);
+    }
+
+}
