6a982c179c4b63f858991bfff902c19ca0514832
kos-vkg <kvertenov@mail.ru>
2020-11-25 13:18:53 +0300

    error correction 1

--- src/Мain.java
+++ src/Мain.java
@@ -1,10 +1,8 @@
 public class Мain {
     public static void main(String[] args) {
-        int bonus_for_mile =  20;
-        long ticket_price = 20000;
-        long bonus = ticket_price / bonus_for_mile;
-
+        int bonusformile = 20;
+        long ticketprice = 20000;
+        long bonus = ticketprice / bonusformile;
         System.out.println(bonus);
     }
-
 }
